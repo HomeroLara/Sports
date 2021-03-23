5c7d025df81316cf91478fee94c709686d8ce39b
Homero Lara <homero.lara@gmail.com>
2021-03-22 21:33:37 -0500

    added images to game template

--- Clients/Sports.Core.Android/Resources/Resource.designer.cs
+++ Clients/Sports.Core.Android/Resources/Resource.designer.cs
@@ -20593,73 +20593,76 @@ namespace Sports.Droid
 			public const int nba_sac = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int nba_tor = 2131165344;
+			public const int nba_sas = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int nba_uta = 2131165345;
+			public const int nba_tor = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int nba_was = 2131165346;
+			public const int nba_uta = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int nfl_logo = 2131165347;
+			public const int nba_was = 2131165347;
 			
 			// aapt resource value: 0x7F0700A4
-			public const int nhl_logo = 2131165348;
+			public const int nfl_logo = 2131165348;
 			
 			// aapt resource value: 0x7F0700A5
-			public const int notification_action_background = 2131165349;
+			public const int nhl_logo = 2131165349;
 			
 			// aapt resource value: 0x7F0700A6
-			public const int notification_bg = 2131165350;
+			public const int notification_action_background = 2131165350;
 			
 			// aapt resource value: 0x7F0700A7
-			public const int notification_bg_low = 2131165351;
+			public const int notification_bg = 2131165351;
 			
 			// aapt resource value: 0x7F0700A8
-			public const int notification_bg_low_normal = 2131165352;
+			public const int notification_bg_low = 2131165352;
 			
 			// aapt resource value: 0x7F0700A9
-			public const int notification_bg_low_pressed = 2131165353;
+			public const int notification_bg_low_normal = 2131165353;
 			
 			// aapt resource value: 0x7F0700AA
-			public const int notification_bg_normal = 2131165354;
+			public const int notification_bg_low_pressed = 2131165354;
 			
 			// aapt resource value: 0x7F0700AB
-			public const int notification_bg_normal_pressed = 2131165355;
+			public const int notification_bg_normal = 2131165355;
 			
 			// aapt resource value: 0x7F0700AC
-			public const int notification_icon_background = 2131165356;
+			public const int notification_bg_normal_pressed = 2131165356;
 			
 			// aapt resource value: 0x7F0700AD
-			public const int notification_template_icon_bg = 2131165357;
+			public const int notification_icon_background = 2131165357;
 			
 			// aapt resource value: 0x7F0700AE
-			public const int notification_template_icon_low_bg = 2131165358;
+			public const int notification_template_icon_bg = 2131165358;
 			
 			// aapt resource value: 0x7F0700AF
-			public const int notification_tile_bg = 2131165359;
+			public const int notification_template_icon_low_bg = 2131165359;
 			
 			// aapt resource value: 0x7F0700B0
-			public const int notify_panel_notification_icon_bg = 2131165360;
+			public const int notification_tile_bg = 2131165360;
 			
 			// aapt resource value: 0x7F0700B1
-			public const int spondoo_white = 2131165361;
+			public const int notify_panel_notification_icon_bg = 2131165361;
 			
 			// aapt resource value: 0x7F0700B2
-			public const int test_custom_background = 2131165362;
+			public const int spondoo_white = 2131165362;
 			
 			// aapt resource value: 0x7F0700B3
-			public const int tooltip_frame_dark = 2131165363;
+			public const int test_custom_background = 2131165363;
 			
 			// aapt resource value: 0x7F0700B4
-			public const int tooltip_frame_light = 2131165364;
+			public const int tooltip_frame_dark = 2131165364;
 			
 			// aapt resource value: 0x7F0700B5
-			public const int ufc_logo = 2131165365;
+			public const int tooltip_frame_light = 2131165365;
 			
 			// aapt resource value: 0x7F0700B6
-			public const int xamarin_logo = 2131165366;
+			public const int ufc_logo = 2131165366;
+			
+			// aapt resource value: 0x7F0700B7
+			public const int xamarin_logo = 2131165367;
 			
 			static Drawable()
 			{
