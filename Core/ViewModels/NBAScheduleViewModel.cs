e4fb821a8206f6e444236862f77d1f0d8e8606eb
Homero Lara <homero.lara@gmail.com>
2021-03-26 22:47:03 -0500

    more UI changes

--- Core/ViewModels/NBAScheduleViewModel.cs
+++ Core/ViewModels/NBAScheduleViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using AsyncAwaitBestPractices.MVVM;
 using Sports.Core.Models.Sports.NBA;
+using Sports.Core.Helpers;
 
 namespace Sports.Core.ViewModels
 {
@@ -13,6 +14,7 @@ namespace Sports.Core.ViewModels
         #region PRIVATE MEMBERS
         private readonly INBASportService _nbaSportsService;
         private ObservableCollection<Game> _games;
+        private ObservableCollection<DateModel> _dates;
         private List<Team> _teams;
         #endregion
 
@@ -26,6 +28,16 @@ namespace Sports.Core.ViewModels
                 OnPropertyChanged();
             }
         }
+
+        public ObservableCollection<DateModel> Dates
+        {
+            get => _dates;
+            set
+            {
+                _dates = value;
+                OnPropertyChanged();
+            }
+        }
         #endregion
 
         #region COMMANDS
@@ -36,6 +48,7 @@ namespace Sports.Core.ViewModels
         public NBAScheduleViewModel(INBASportService nbaSportsService)
         {
             _nbaSportsService = nbaSportsService;
+            LoadDates();
         }
         #endregion
 
@@ -59,6 +72,30 @@ namespace Sports.Core.ViewModels
         #endregion
 
         #region METHODS
+        private void LoadDates()
+        {
+            var dates = new List<DateModel>();
+            var dateInit = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+            var dateEnd = new DateTime(dateInit.Year, dateInit.Month, DateTime.DaysInMonth(dateInit.Year, dateInit.Month));
+
+            for (int i = 1; i <= dateEnd.Day; i++)
+            {
+                dates.Add(new DateModel()
+                {
+                    Day = string.Format("{0:00}", i),
+                    Month = dateInit.ToString("MMM").FirstLetterUpperCase(),
+                    DayWeek = new DateTime(dateInit.Year, dateInit.Month, i).DayOfWeek.ToString().Substring(0, 3),
+                    Date = new DateTime(dateInit.Year, dateInit.Month, i),
+                    Selected = i == DateTime.Today.Day,
+                    BackgroundColor = i == DateTime.Today.Day ? "#252A37" : "Transparent",
+                    FrameBorderColor = i == DateTime.Today.Day ? "#FFFFFF" : "#252A37",
+                    TextColor = i == DateTime.Today.Day ? "#FFFFFF" : "#252A37",
+                });
+            }
+
+            Dates = new ObservableCollection<DateModel>(dates);
+        }
+
         private async Task GameTapped(Game game)
         {
             var g = game;
